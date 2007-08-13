0d2cc506d331be604543d1e280a516a2e111f0a4
Marcus <marcus@it.su.se>
2007-08-13 08:10:52 +0000

    added debug print of load path in helm spec.

--- src/test/jruby/helm_server_spec.rb
+++ src/test/jruby/helm_server_spec.rb
@@ -1,5 +1,9 @@
-#$LOAD_PATH << File.expand_path('../../../target/classes')
 require 'java'
+
+$LOAD_PATH << File.expand_path('../../../target/classes')
+
+# debug print of load path
+puts $LOAD_PATH
 
 module HelmServerSpec
   include_class 'se.su.it.helm.HelmServer'
@@ -10,6 +14,7 @@ module HelmServerSpec
       @helm = HelmServer.new(A_PORT)
     end
     it "should return a version string" do
+      puts $CLASSPATH
       EXPECTED = 'helm-0.0.1'
       @helm.version.should eql(EXPECTED)
     end
@@ -18,4 +23,3 @@ module HelmServerSpec
     end
   end
 end
-
